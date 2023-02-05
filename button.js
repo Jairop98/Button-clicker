1125143896efccb0a91b1dbb8e7c024a8aa819bd
Jairop98 <jairop98@yahoo.com>
2023-02-04 18:17:30 -0800

    first commit

---
+++ button.js
@@ -0,0 +1,15 @@
+function turnLogout(element){
+    element.innerText="Logout";
+}
+function turn14likes (element){
+    element.innerText="14 likes";
+}
+function turn38likes (element){
+    element.innerText="38 likes";
+}
+function hide (element){
+    element.remove();
+}
+function message() {
+    alert("Ninja was liked!");
+}
